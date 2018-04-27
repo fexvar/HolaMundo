5f8b9df87e39304846ca0efa552133633d17c9ed
Felix Vargas Vela <felixvar91@gmail.com>
2018-04-27 09:19:34 -0400

    Se anadio el archivo HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+/** clase de ejemplos con java
+* imorime el mensaje (hola mundo)
+*/
+public class HolaMundo{
+
+/**no se ve 
+*/
+	public static void main (String[] args){
+	System.out.println("Hola Mundo");	
+	}
+}
